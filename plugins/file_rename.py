5fd7aadfd8a38b102f744404a5b92ac33a7f118a
acxanime <acxanime07@gmail.com>
2025-02-28 19:44:18 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -32,7 +32,7 @@ async def doc(bot, update):
     type = update.data.split("_")[1]
     user_id = int(update.message.chat.id)
     new_name = update.message.text
-    new_filename = new_name.split(":-")[1]
+    new_filename = new_name.split("<b>:-</b>")[1]
     file_path = f"downloads/{user_id}{time.time()}/{new_filename}"
     file = update.message.reply_to_message
     ms = await update.message.edit("⚠️__Please wait...__\n__Downloading file to my server...__")
